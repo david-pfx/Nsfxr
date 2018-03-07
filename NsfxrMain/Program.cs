a0f42901960e8800e413b44b30977d4d1a319e12
david <david@yorkage.com>
2018-03-07 21:25:25 +1100

    Original commit for Github upload.

---
+++ NsfxrMain/Program.cs
@@ -0,0 +1,78 @@
+﻿/// Puzzlang is a pattern matching language for abstract games and puzzles. See http://www.polyomino.com/puzzlang.
+///
+/// Copyright © Polyomino Games 2018. All rights reserved.
+/// 
+/// This is free software. You are free to use it, modify it and/or 
+/// distribute it as set out in the licence at http://www.polyomino.com/licence.
+/// You should have received a copy of the licence with the software.
+/// 
+/// This software is distributed in the hope that it will be useful, but with
+/// absolutely no warranty, express or implied. See the licence for details.
+/// 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NsfxrLib;
+using DOLE;
+using System.IO;
+
+namespace Nsfxr {
+  class Program {
+
+    const string Version = "Nsfxr 1.0b";
+    static string _help = "Nsfxr <seed> [<output> /verbose]";
+    static TextWriter _out = Console.Out;
+    static bool _xdebug = false;
+
+    static readonly Dictionary<string, Action<string>> _options
+      = new Dictionary<string, Action<string>>(StringComparer.CurrentCultureIgnoreCase) {
+        { "verbose",   (a) => { Logger.Level = 1; } },
+        { "xdebug",   (a) => { _xdebug = true; } },
+      };
+
+    static readonly Dictionary<int, Func<RNG, Patch>> _seedlookup = new Dictionary<int, Func<RNG, Patch>> {
+      { 0, r=>Global.PickupCoin(r) },
+      { 1, r=>Global.LaserShoot(r) },
+      { 2, r=>Global.Explosion(r) },
+      { 3, r=>Global.PowerUp(r) },
+      { 4, r=>Global.HitHurt(r) },
+      { 5, r=>Global.Jump(r) },
+      { 6, r=>Global.BlipSelect(r) },
+      { 7, r=>Global.PushSound(r) },
+      { 8, r=>Global.Random(r) },
+      { 9, r=>Global.Bird(r) },
+    };
+
+    static void Main(string[] args) {
+      _out.WriteLine(Version);
+      var options = OptionParser.Create(_options, _help);
+      if (!options.Parse(args))
+        return;
+      if (_xdebug) {
+        options.Parse(new string[] { "/verbose", "36772507" });  // crate move
+        //options.Parse(new string[] { "/verbose", "36772507", "test.wav" });  // crate move
+      }
+      if (options.PathsCount < 1)
+        _out.WriteLine("Must specify seed");
+      else {
+        var nsound = options.GetPath(0).SafeIntParse() ?? 0;
+        var filename = options.GetPath(1) ?? options.GetPath(0);
+        if (!Path.HasExtension(filename)) filename = Path.ChangeExtension(filename, ".wav");
+        if (!_seedlookup.ContainsKey(nsound % 100))
+          _out.WriteLine("Invalid seed");
+        else DoGenerate(nsound, filename);
+      }
+    }
+
+    static void DoGenerate(int nseed, string pathname) {
+      _out.WriteLine("Writing sound '{0}' to {1}", nseed, pathname);
+      var rng = new RNG(nseed / 100);
+      var patch = _seedlookup[nseed % 100](rng);
+      Logger.WriteLine(1, "*** seed is {0}\n{1}", nseed, patch);
+      var gen = new Generator(patch);
+      var samps = gen.Generate();
+      Logger.WriteLine(1, "Generated {0} samples.", samps.Count);
+      Global.SaveWAV(samps, pathname);
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# Nsfxr
-Procedure sound generator ported from Pzfxr
+# Nxfxr procedural sound generator 
+
+Nsfxr is a procedural sound generator ported to C# from Pzfxr. 
+It builds with .NET 3.5, which makes it easy to use with Unity.
+
+The project consists of two parts.
+
+1. A library DLL containing all the generation logic and callable by a simple public API. 
+This can be used directly in Unity (not yet fully tested).
+
+2. A console executable that generates sounds and writes out WAV files. 
+By default the file name is derived from the seed, or it can be specified on the command line.
+
+Run it for help about what it does.
+
+## Credits
+
+Pzfxr is by Nathan Whitehead. 
+See https://github.com/nwhitehead/pzfxr for prior history. 
+Directly based on
+the [PuzzleScript](https://www.puzzlescript.net/) code in
+file `js/sfxr.js` by increpare. The PuzzleScript
+sound generation functions are based on [BFXR](https://www.bfxr.net/)
+by increpare, which is based on the original
+[SFXR](http://www.drpetter.se/project_sfxr.html) by Thomas Petersson.
+
+## Licence
+
+The licence is a moot point. 
+The command line program is entirely my work, and that is under the Polyomino Free Software Licence.
+However, most of the work is not mine, and any licence wording in the original source code has been lost along the way.
+If you really care you might have some work to do to trace the lineage.
+
+
